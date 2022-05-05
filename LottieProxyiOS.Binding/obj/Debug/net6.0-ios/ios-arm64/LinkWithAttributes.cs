20f8710158bf353ab326b4ab4b2b953fe89616f4
Csaba Huszar <csab.huszar@gmail.com>
2022-05-05 10:26:29 +0200

    remove bin obj

--- LottieProxyiOS.Binding/obj/Debug/net6.0-ios/ios-arm64/LinkWithAttributes.cs
+++
@@ -1,2 +0,0 @@
-[assembly: ObjCRuntime.LinkWith ("LottieProxyiOS.framework", SmartLink = true, Frameworks = "Foundation UIKit")]
-[assembly: ObjCRuntime.LinkWith ("Lottie.xcframework", SmartLink = true, Frameworks = "Foundation UIKit")]
